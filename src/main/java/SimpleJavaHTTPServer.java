3fd119863bc50d3454250e676f8342e1556c33ed
RyoheiYamamoto <ryohei.yamamoto@bizreach.jp>
2017-04-27 20:54:44 +0900

    STEP2 全く進んでないけど、プッシュします。

--- src/main/java/SimpleJavaHTTPServer.java
+++ src/main/java/SimpleJavaHTTPServer.java
@@ -12,7 +12,7 @@ public class SimpleJavaHTTPServer {
     private static Logger logger = new Logger(SimpleJavaHTTPServer.class.getSimpleName());
 
     public static void main(String[] args) throws IOException {
-
+        // TODO hakiba 全く進んでないけど、プッシュします。 by anyone (2017/04/27)
         logger.log("Hello, I'll be an HTTP Server!");
     }
 }
